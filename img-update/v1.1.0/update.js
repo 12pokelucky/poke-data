8c9c607180102af061d90dcab0f2d71f72837453
12LuckyDev <12luckydev@gmail.com>
2023-03-06 14:38:32 +0100

    Change names of regional forms _rfx -> _id

---
+++ img-update/v1.1.0/update.js
@@ -0,0 +1,22 @@
+const fs = require("fs");
+
+const list = fs.readdirSync("../../img_96");
+const cache = {};
+
+list.forEach((name) => {
+	if (name.includes("_rf")) {
+		const [number, rf] = name.split("_");
+		const _rf = rf.replace(".png", "");
+		let id = 1;
+		if (cache[number] && !name.includes("_f")) {
+			id = cache[number] + 1;
+			cache[number] = id;
+		} else {
+			cache[number] = 1;
+		}
+
+		const newName = name.replace(_rf, id);
+		console.log(newName);
+		fs.renameSync(`../../img_96/${name}`, `../../img_96/${newName}`);
+	}
+});
